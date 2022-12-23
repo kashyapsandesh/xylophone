081a23c82c0a973a9c20a7fab098e6dde9edca66
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2022-12-23 20:24:33 +0545

    audio play play feature added

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:xylophone/pages/homepage.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "xylophone",
       home: HomePage(),
     );
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,5 @@
-import 'dart:developer';
-import 'dart:typed_data';
-
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,54 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int maxduration = 100;
-  int currentpos = 0;
-  String currentpostlabel = "00:00";
-  String audioasset = "assets/audio/note1.wav";
-  bool isplaying = false;
-  bool audioplayed = false;
-  late Uint8List audiobytes;
-
   AudioPlayer player = AudioPlayer();
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero, () async {
-      ByteData bytes =
-          await rootBundle.load(audioasset); //load audio from assets
-      audiobytes =
-          bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
-      //convert ByteData to Uint8List
-      log(audiobytes.toString());
-
-      player.onDurationChanged.listen((Duration d) {
-        //get the duration of audio
-        maxduration = d.inMilliseconds;
-        setState(() {});
-      });
-
-      player.onPositionChanged.listen((Duration p) {
-        currentpos =
-            p.inMilliseconds; //get the current position of playing audio
-
-        //generating the duration label
-        int shours = Duration(milliseconds: currentpos).inHours;
-        int sminutes = Duration(milliseconds: currentpos).inMinutes;
-        int sseconds = Duration(milliseconds: currentpos).inSeconds;
-
-        int rhours = shours;
-        int rminutes = sminutes - (shours * 60);
-        int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
-
-        currentpostlabel = "$rhours:$rminutes:$rseconds";
-
-        setState(() {
-          //refresh the UI
-        });
-      });
-    });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,45 +17,30 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Xylophone'),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            if (!isplaying && !audioplayed) {
-              int result = await player.play(audiobytes);
-              if (result == 1) {
-                //play success
-                setState(() {
-                  isplaying = true;
-                  audioplayed = true;
-                });
-              } else {
-                print("Error while playing audio.");
-              }
-            } else if (audioplayed && !isplaying) {
-              int result = await player.resume();
-              if (result == 1) {
-                //resume success
-                setState(() {
-                  isplaying = true;
-                  audioplayed = true;
-                });
-              } else {
-                print("Error on resume audio.");
-              }
-            } else {
-              int result = await player.pause();
-              if (result == 1) {
-                //pause success
-                setState(() {
-                  isplaying = false;
-                });
-              } else {
-                print("Error on pause audio.");
-              }
-            }
-          },
-          child: Text("Click Me"),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              // await player.setSourceUrl();
+              String urlsource =
+                  'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3';
+
+              await player.play(UrlSource(urlsource));
+            },
+            child: const Text("Click Me"),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              // await player.setSourceUrl();
+              String localsource = 'sounds/note1.wav';
+
+              await player.play(DeviceFileSource(localsource));
+            },
+            child: const Text("Click Me"),
+          ),
+        ],
       ),
     );
   }
